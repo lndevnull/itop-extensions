29b265af25bf0823d0f2f614cd14461f47858805
lndevnull <cory.meyer@gmail.com>
2014-08-12 12:23:04 -0500

    Initial addition of dbcluster-ci

---
+++ dbcluster-ci/README.md
@@ -0,0 +1,4 @@
+dbcluster-ci
+===============
+
+DBCluster Ci Extension for iTop v2.0.   It is an attempt to create a flexable object to define OracleRAC/MSSQL clustered environments in iTop.
---
+++ dbcluster-ci/en.dict.dbcluster-ci.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:DBCluster' => 'DB Cluster',
+	'Class:DBCluster+' => '',
+	'Class:lnkDBServerToDBCluster' => 'DB Cluster',
+	'Class:lnkDBServerToDBCluster+' => '',
+	'Class:DatabaseSchema/Attribute:dbcluster_id' => 'Database Cluster',
+	'Class:DatabaseSchema/Attribute:dbcluster_id+' => '',
+	'Class:DBCluster/Attribute:dbserver_list' => 'DB Servers',
+	'Class:DBCluster/Attribute:dbserver_list+' => '',
+	'Class:DBCluster/Attribute:dbschema_list' => 'Associated Schemas',
+	'Class:DBCluster/Attribute:dbschema_list+' => '',
+	'Class:DBServer/Attribute:dbcluster_list' => 'DB Clusters',
+	'Class:DBServer/Attribute:dbcluster_list+' => '',
+	'Class:DatabaseSchema/Attribute:webapplications_list' => 'Web Applications',
+	'Class:DatabaseSchema/Attribute:webapplications_list+' => '',
+	'Class:WebApplication/Attribute:dbinstances_list' => 'Database Instances',
+	'Class:WebApplication/Attribute:dbinstances_list+' => '',
+	'Class:DBCluster/Attribute:volume_list' => 'Logical Volumes',	
+	'Class:DBCluster/Attribute:volume_list+' => '',	
+));
+?>
